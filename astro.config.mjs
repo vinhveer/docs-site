8d35962e9483b9f9d179c7862eafc1aae1df5bd4
Nguyen Quang Vinh <63202399+vinhveer@users.noreply.github.com>
2025-06-10 16:53:05 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://docs-site.github.io',
+  base: '/docs-site',
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()],
   },
